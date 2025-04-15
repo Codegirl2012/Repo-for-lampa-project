27c6f618ae2682f10f72adfda26f9e26a6b68934
Codegirl2012 <zehraezizli2@gmail.com>
2025-04-15 20:31:27 +0400

    ilhukhkh

---
+++ app.js
@@ -0,0 +1,7 @@
+function LampOFF() {
+    document.getElementById("lamp").src = "./images/pic_bulboff.gif"; 
+}
+
+function LampON() {
+    document.getElementById("lamp").src = "./images/pic_bulbon.gif";
+}
